d83c3d58325f6753bf0cdfb5bf7af9e8afbe33f1
vishwajit kumar singh <vishwa3556@gmail.com>
2022-02-14 00:12:59 +0530

    update

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,42 +1,116 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import Web3 from "web3";
-import { TODO_LIST_ABI, TODO_LIST_ADDRESS } from "../config";
+import { OWNERSHIP_ABI, OWNERSHIP_ADDRESS } from "../config";
+/**
+ * get all the items from the map in the contract.
+ * fetching the different address with metamask account.
+ * ask for owner ship with only a button and cur account of metamask account as msg.sender.
+ * show the ownership request.
+*/
+const web3 = new Web3(new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545"));
 
-class Homepage extends Component{
-	constructor(props){
-		super(props);
-		this.state={
-			account:"",
-			taskCount:0
+const contract = new web3.eth.Contract(OWNERSHIP_ABI, OWNERSHIP_ADDRESS);
+
+const Homepage = () => {
+	const [account, onSetAccount] = useState("");
+	const [ownerShipType, onChangeOwnerShipType] = useState("");
+	const [id, onChangePublicId] = useState(0);
+	const [owner, onChangeOwner] = useState("");
+	const [newOwnerType, onNewOwnerType] = useState("");
+	const [pdtId, onWhichId] = useState(0);
+	const [newOwner, onNewOwner] = useState("");
+	const [qID, onqId] = useState(0);
+	
+	useEffect(() => {
+		if(window.ethereum){
+			window.Web3 = new Web3(window.ethereum);
+			window.ethereum.enable();
+			console.log("etherium detected")
+			async function getAccounts(){
+				const accounts = await web3.eth.getAccounts();
+				onSetAccount(accounts[0]);
+			}
+			getAccounts();
 		}
+	},[])
+
+	const onChangeAddress = (e) => {
+		e.preventDefault();
+		onChangeOwner(e.target.value);
 	}
-	componentDidMount(){
-		this.loadBlockChain();
+	const onChangeOwnershipId = (e) => {
+		e.preventDefault();
+		onChangeOwnerShipType(e.target.value);
 	}
-	async loadBlockChain(){
-		const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
-		const network = await web3.eth.net.getNetworkType()
-		console.log("network ", network);
-		const account = await web3.eth.getAccounts()
-		this.setState({
-			account: account
-		})
-		const todoList = new web3.eth.Contract(TODO_LIST_ABI, TODO_LIST_ADDRESS)
-		const taskCount = await todoList.methods.taskCount().call()
-		this.setState({
-			taskCount: taskCount
-		})
+	const onChangePdtId = (e) => {
+		e.preventDefault();
+		onChangePublicId(e.target.value);
 	}
-	render(){
-		return(
-			<div>
-				Loading Blockchain
-				{this.state.account}
-				<br/>
-				{this.state.taskCount}
-			</div>
-		)
+	const onCreateItem = async(e) => {
+		e.preventDefault();
+		console.log(account);
+		const ID = parseInt(id);
+		const response = await contract.methods.createItem(ownerShipType, ID).send({from: account, gas: 3000000});
+		console.log(response);
 	}
+	const getVal = async(e) => {
+		e.preventDefault();
+		const account = await web3.eth.accounts[0];
+		console.log(account);
+		contract.methods.val().call().then((res) => console.log(res)).catch(err => console.log(err));
+	}
+	const onChangeAddressshift = (e) => {
+		e.preventDefault();
+		onNewOwner(e.target.value);
+	}
+	const onChangeOwnershipIdshift = (e) => {
+		e.preventDefault();
+		onNewOwnerType(e.target.value);
+	}
+	const onChangePdtIdshift = (e) => {
+		e.preventDefault();
+		onWhichId(e.target.value);
+	}
+	const onShiftOwner = async(e) => {
+		e.preventDefault();
+		const ID = parseInt(id);
+		const response = await contract.methods.askOwnerShip(ID,ownerShipType).send({from: '0x8287cB594058009292D85A6922bd42e296472d3B', gas: 3000000})
+		console.log(response);
+	}
+	const onQuery = async(e) => {
+		e.preventDefault();
+		const response = await contract.methods.publicView(qID).send({from: account, gas: 3000000})
+		console.log(response);
+		const history = await contract.methods.showOwnerShipHistory(qID).send({from: account, gas: 3000000})
+		console.log(history);
+	}
+	const queryId = (e) => {
+		e.preventDefault();
+		onqId(e.target.value);
+	}
+	return(
+		<div>
+			{account}
+			<br />
+			<form onSubmit={onCreateItem}>
+			<input onChange={onChangeAddress} value={owner} placeholder="Enter address" />
+			<input onChange={onChangeOwnershipId} value={ownerShipType} placeholder="EnterOwnerType" />
+			<input onChange={onChangePdtId} value={id} placeholder="pdtID" />
+			<button>submit</button>
+			</form>
+			<button onClick={getVal}>GetVal</button>
+			<form onSubmit={onShiftOwner}>
+			<input onChange={onChangeAddressshift}  vaue ={newOwner}placeholder="Enter address" />
+			<input onChange={onChangeOwnershipIdshift} value={newOwnerType} placeholder="EnterOwnerType" />
+			<input onChange={onChangePdtIdshift}  value = {pdtId} placeholder="pdtID" />
+			<button>changeOwner</button>
+			</form>
+			<form onSubmit={onQuery}>
+				<input onChange={queryId} value={qID} placeholder="queryID"/>
+				<button>query</button>    
+			</form>		    
+		</div>
+	)
 }
 
 export default Homepage;
--- src/config.js
+++ src/config.js
@@ -1,131 +1,187 @@
-export const TODO_LIST_ADDRESS = '0x8C458391f504EAB9a8AE958Bc889B8a403632A7d'
-export const TODO_LIST_ABI=[
-	
+export const OWNERSHIP_ADDRESS = '0xa3fAc3372Add06Ccb095fFF64f403c62209632ad'
+export const OWNERSHIP_ABI=[
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "_to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "_from",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "ownershipType",
+				"type": "uint256"
+			}
+		],
+		"name": "changedOwnership",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "_to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "_from",
+				"type": "address"
+			}
+		],
+		"name": "shiftBetweenOwner",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "Items",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "publicID",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "ownershipType",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_idx",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_ownershipType",
+				"type": "uint256"
+			}
+		],
+		"name": "askOwnerShip",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_ownershipType",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_id",
+				"type": "uint256"
+			}
+		],
+		"name": "createItem",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_idItem",
+				"type": "uint256"
+			}
+		],
+		"name": "getOwner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_id",
+				"type": "uint256"
+			}
+		],
+		"name": "publicView",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_idx",
+				"type": "uint256"
+			}
+		],
+		"name": "showOwnerShipHistory",
+		"outputs": [
+			{
+				"internalType": "address[]",
+				"name": "",
+				"type": "address[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
 	{
 		"inputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "constructor"
-	   },
-	   {
-		"anonymous": false,
-		"inputs": [
-		  {
-		    "indexed": false,
-		    "internalType": "uint256",
-		    "name": "id",
-		    "type": "uint256"
-		  },
-		  {
-		    "indexed": false,
-		    "internalType": "bool",
-		    "name": "completed",
-		    "type": "bool"
-		  }
-		],
-		"name": "TaskCompleted",
-		"type": "event"
-	   },
-	   {
-		"anonymous": false,
-		"inputs": [
-		  {
-		    "indexed": false,
-		    "internalType": "uint256",
-		    "name": "id",
-		    "type": "uint256"
-		  },
-		  {
-		    "indexed": false,
-		    "internalType": "string",
-		    "name": "content",
-		    "type": "string"
-		  },
-		  {
-		    "indexed": false,
-		    "internalType": "bool",
-		    "name": "completed",
-		    "type": "bool"
-		  }
-		],
-		"name": "TaskCreated",
-		"type": "event"
-	   },
-	   {
-		"constant": true,
-		"inputs": [],
-		"name": "taskCount",
+		"name": "val",
 		"outputs": [
-		  {
-		    "internalType": "uint256",
-		    "name": "",
-		    "type": "uint256"
-		  }
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
 		],
-		"payable": false,
 		"stateMutability": "view",
 		"type": "function"
-	   },
-	   {
-		"constant": true,
-		"inputs": [
-		  {
-		    "internalType": "uint256",
-		    "name": "",
-		    "type": "uint256"
-		  }
-		],
-		"name": "tasks",
-		"outputs": [
-		  {
-		    "internalType": "uint256",
-		    "name": "id",
-		    "type": "uint256"
-		  },
-		  {
-		    "internalType": "string",
-		    "name": "content",
-		    "type": "string"
-		  },
-		  {
-		    "internalType": "bool",
-		    "name": "completed",
-		    "type": "bool"
-		  }
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	   },
-	   {
-		"constant": false,
-		"inputs": [
-		  {
-		    "internalType": "string",
-		    "name": "_content",
-		    "type": "string"
-		  }
-		],
-		"name": "createTask",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	   },
-	   {
-		"constant": false,
-		"inputs": [
-		  {
-		    "internalType": "uint256",
-		    "name": "_id",
-		    "type": "uint256"
-		  }
-		],
-		"name": "toggleCompleted",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	   }
-	 
+	}
 ]
